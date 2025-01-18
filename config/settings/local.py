24037309efcade2440867e9c707be024f9cf0a31
Paolo Greppi <paolo.greppi@libpf.com>
2025-01-18 12:30:40 +0100

    add flash to allowed hosts

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
     default="gaLT8kp1IOZJrhxgQ3NF5Sz4dq7c6lYCuHAAEtmp6vXhlpZhpTkXisUqNxAilnAh",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "flash"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
