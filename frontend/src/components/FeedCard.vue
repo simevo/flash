10584751cff7299edc08798f25340d0191654aa5
Paolo Greppi <paolo.greppi@libpf.com>
2025-04-28 06:48:05 +0200

    fix responsive feed list

--- frontend/src/components/FeedCard.vue
+++ frontend/src/components/FeedCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router"
 import RatingToolbar from "./RatingToolbar.vue"
+import { secondsToString } from "./sts"
 
 import type { components } from "../generated/schema.d.ts"
 type Feed = components["schemas"]["Feed"]
@@ -12,35 +13,53 @@ defineProps<{
 </script>
 
 <template>
-  <div :id="'feed_' + feed.id" class="card mb-3 p-1">
-    <div class="d-flex flex-row justify-content-between align-items-center">
-      <img
-        style="margin: 5px"
-        width="50"
-        height="50"
-        :src="`https://notizie.calomelano.it/${feed.icon}`"
-        alt="feed logo"
-      />
-      <RouterLink
-        class="text-decoration-none card-body"
-        :to="'/feed/' + feed.id"
-        :title="'vai a tutti gli articoli di ' + feed.title"
-        :style="{ cursor: 'pointer' }"
-      >
-        <h5>{{ feed.title }} ({{ feed.article_count }} articoli)</h5>
-        <span class="text-decoration-underline">{{ feed.homepage }}</span>
-      </RouterLink>
-      <div class="text-end">
-        <span v-for="tag in feed.tags" :key="tag" class="badge text-bg-secondary m-1 p-2">
-          {{ tag }}
-        </span>
+  <div class="card mb-3" :id="'feed_' + feed.id">
+    <div class="row g-0">
+      <div class="col-md-1 p-2 text-center align-center">
+        <RouterLink
+          class="text-decoration-none"
+          :to="'/feed/' + feed.id"
+          :title="'vai a tutti gli articoli di ' + feed.title"
+          :style="{ cursor: 'pointer' }"
+        >
+          <img
+            width="100"
+            :src="`https://notizie.calomelano.it/${feed.icon}`"
+            class="img-fluid border"
+            alt="feed logo"
+          />
+        </RouterLink>
+      </div>
+      <div class="col-md-11">
+        <div class="card-body">
+          <RouterLink
+            class="text-decoration-none"
+            :to="'/feed/' + feed.id"
+            :title="'vai a tutti gli articoli di ' + feed.title"
+            :style="{ cursor: 'pointer' }"
+          >
+            <h5 class="card-title">{{ feed.title }} ({{ feed.article_count }} articoli)</h5>
+            <span class="card-text text-decoration-underline">{{ feed.homepage }}</span>
+          </RouterLink>
+          <p class="card-text my-1">
+            <span v-for="tag in feed.tags" :key="tag" class="badge text-bg-secondary m-1 p-2">
+              {{ tag }}
+            </span>
+          </p>
+          <p class="card-text" v-if="feed.last_polled_epoch">
+            <small class="text-body-secondary">
+              Ultimo aggiornamento
+              {{ secondsToString(new Date().getTime() / 1000 - feed.last_polled_epoch) }}
+            </small>
+            <RatingToolbar
+              class="float-end mb-2"
+              :id="feed.id"
+              endpoint="/api/user-feeds/"
+              v-bind:item="feed"
+            ></RatingToolbar>
+          </p>
+        </div>
       </div>
     </div>
-    <RatingToolbar
-      class="d-flex justify-content-end mb-1"
-      :id="feed.id"
-      endpoint="/api/user-feeds/"
-      v-bind:item="feed"
-    ></RatingToolbar>
   </div>
 </template>
