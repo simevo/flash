6a2550e2406f7ad44c41283600f6e4215b05e6cb
Paolo Greppi <paolo.greppi@libpf.com>
2025-06-01 20:10:20 +0200

    fix backend tests

--- flash/users/tests/test_drf_views.py
+++ flash/users/tests/test_drf_views.py
@@ -29,6 +29,7 @@ class TestUserViewSet:
         response = view.me(request)  # type: ignore[call-arg, arg-type, misc]
 
         assert response.data == {
+            "id": user.pk,
             "username": user.username,
             "url": f"http://testserver/api/users/{user.username}/",
             "name": user.name,
