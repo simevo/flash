57d37396b4806ab361ccbdeacf71b6308b9dee8b
Paolo Greppi <paolo.greppi@libpf.com>
2025-07-21 06:31:04 +0200

    fix jules code
    closes #49

--- frontend/src/components/ListFavs.vue
+++ frontend/src/components/ListFavs.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { fetch_wrapper } from "../utils"
-import { computed, onActivated, onDeactivated, onMounted, onUnmounted, ref, type Ref } from "vue"
+import { onActivated, onDeactivated, onMounted, onUnmounted, ref, type Ref } from "vue"
 import { toast } from "vue3-toastify"
 
 import ArticleCard from "./ArticleCard.vue"
@@ -20,17 +20,11 @@ defineProps<{
   feed_dict: { [key: number]: FeedSerializerSimple }
 }>()
 
-const not_filtering = computed(() => true)
-
 function sorting_key(a: ArticleRead) {
-  if (not_filtering.value) {
-    // client-side deterministically perturbed chronological order, assuming:
-    // - 200 feeds and 2000 articles/day (on average 10 articles per day and feed)
-    // - average length 4000 chars
-    return a.stamp / 3600 / 24 - a.feed - a.length / 20
-  } else {
-    return a.stamp
-  }
+  // client-side deterministically perturbed chronological order, assuming:
+  // - 200 feeds and 2000 articles/day (on average 10 articles per day and feed)
+  // - average length 4000 chars
+  return a.stamp / 3600 / 24 - a.feed - a.length / 20
 }
 
 function sort(articles: ArticleRead[]) {
